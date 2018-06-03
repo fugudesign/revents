fe4738e597a11568904083a10e1a6d326ed3fff4
Fugu Design <fugudesign@mac.com>
2018-06-03 17:13:46 +0200

    End of Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import {Container} from 'semantic-ui-react'
+import EvenDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-events</h1>
+        <NavBar/>
+        <Container className="main">
+          <EvenDashboard/>
+        </Container>
       </div>
     );
   }
